c3f3eb68ce6a9500a84439e75fe39801dbd4a1ca
Christian <Christian@bitthinker.com>
2014-11-19 15:04:16 +0300

    add basic logic for CalculateScore function. use struct instead of point

--- dots-game.go
+++ dots-game.go
@@ -37,25 +37,33 @@ const CLR_N = "\x1b[0m"
 
 const chars  string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+"
 
+type gameBoardNode struct {
+	value int;	// 0, 1, 2 (Empty, User, PC)
+}
+
 func d (debugMsg string) {
 	fmt.Printf("D: ")
 	fmt.Println(debugMsg)
-	time.Sleep(500 * time.Millisecond)
+	time.Sleep(400 * time.Millisecond)
 }
 
-func initGameBoard(size int) (gameBoard [][]int) {
-	d("Init some game field")
+func initGameBoard(size int) (gameBoard [][]gameBoardNode) {
 
 	// Allocate the top-level slice, the same as before.
-	gameBoard = make([][]int, size) // One row per unit of y.
+	gameBoard = make([][]gameBoardNode, size) // One row per unit of y.
 
 	// Allocate one large slice to hold all the pixels.
-	pixels := make([]int, size*size) // Has type []uint8 even though picture is [][]uint8.
+	pixels := make([]gameBoardNode, size*size)
 
 	// Loop over the rows, slicing each row from the front of the remaining pixels slice.
 	for i := range gameBoard {
 		gameBoard[i], pixels = pixels[:size], pixels[size:]
 	}
+
+	gameBoard[0][1].value = 1
+	gameBoard[1][0].value = 1
+	gameBoard[2][1].value = 1
+	gameBoard[1][2].value = 1
 
 	return
 }
@@ -66,9 +74,46 @@ func clear_screen_linux() {
         cmd.Run()
     }
 
-func doUserStep(gameBoard [][]int) {
+/*
+ * name: drawGameBoard
+ * @param
+ * @return
+ *
+ * 0 mean empty cell -- draw fieldEmptyCellChar
+ * 1 mean user  cell -- draw fieldUserCellChar (blue)
+ * 2 mean PC    cell -- draw fieldPCCellChar (red)
+ */
+func drawGameBoard(gameBoard [][]gameBoardNode) {
 
-	var firstIndex int  = 0
+	var length int = len(gameBoard)
+
+	fmt.Printf("    ")
+	for i := 0; i < length; i++ {
+		fmt.Printf("%c ", chars[i])
+    }
+	fmt.Println()
+	for i := range gameBoard {
+		fmt.Printf("%2d  ", i)
+
+		for j:= range gameBoard[i] {
+
+			if fieldEmptyCellId == gameBoard[i][j].value {
+				fmt.Printf("%s ", fieldEmptyCellChar)
+			} else if fieldUserCellId == gameBoard[i][j].value {
+				fmt.Printf("%s%s%s ", CLR_B, fieldUserCellChar, CLR_N)
+			} else if fieldPCCellId == gameBoard[i][j].value {
+				fmt.Printf("%s%s%s ", CLR_R, fieldPCCellChar, CLR_N)
+			}
+		}
+		fmt.Println("")
+	}
+
+	// TODO: Print game scrore. User : xxx; PC: xxx.
+}
+
+func doUserStep(gameBoard [][]gameBoardNode) {
+
+	var firstIndex  int = 0
 	var secondIndex int = 0
 
 	var fineInput int = 0;
@@ -108,48 +153,13 @@ func doUserStep(gameBoard [][]int) {
 }
 
 /*
- * name: drawGameBoard
- * @param
- * @return
- *
- * 0 mean empty cell -- draw fieldEmptyCellChar
- * 1 mean user  cell -- draw fieldUserCellChar (blue)
- * 2 mean PC    cell -- draw fieldPCCellChar (red)
- */
-func drawGameBoard(gameBoard [][]int) {
-
-	var length int = len(gameBoard)
-
-	fmt.Printf("    ")
-	for i := 0; i < length; i++ {
-		fmt.Printf("%c ", chars[i])
-    }
-	fmt.Println()
-	for i := range gameBoard {
-		fmt.Printf("%2d  ", i)
-
-		for j:= range gameBoard[i] {
-
-			if fieldEmptyCellId == gameBoard[i][j] {
-				fmt.Printf("%s ", fieldEmptyCellChar)
-			} else if fieldUserCellId == gameBoard[i][j] {
-				fmt.Printf("%s%s%s ", CLR_B, fieldUserCellChar, CLR_N)
-			} else if fieldPCCellId == gameBoard[i][j] {
-				fmt.Printf("%s%s%s ", CLR_R, fieldPCCellChar, CLR_N)
-			}
-		}
-		fmt.Println("")
-	}
-}
-
-/*
  * return 0 if fine
  * 	  1 if cell not empty already or error occured
  */
-func doGameStep(gameBoard [][]int, x int, y int, symbol int) (result int) {
+func doGameStep(gameBoard [][]gameBoardNode, x int, y int, symbol int) (result int) {
 
-	if fieldEmptyCellId == gameBoard[x][y] {
-		gameBoard[x][y] = symbol
+	if fieldEmptyCellId == gameBoard[x][y].value {
+		gameBoard[x][y].value = symbol
 		result = 0
 	} else {
 		result = 1
@@ -158,22 +168,16 @@ func doGameStep(gameBoard [][]int, x int, y int, symbol int) (result int) {
 	return
 }
 
-func getWinner(gameBoard [][]int) (winner int) {
-	d("get winner")
-
-	winner = 0
-	return
-}
 
 
-func doAIStep(gameBoard [][]int) {
+func doAIStep(gameBoard [][]gameBoardNode) {
 	d("do AI step")
 
 	var stepIsDone = 0
 	for 1 != stepIsDone {
 		// suppose that field is square/rectangle
-		var x int = rand.Intn(len(gameBoard))
-		var y int = rand.Intn(len(gameBoard[0]))
+		var x int = rand.Intn(gameBoardSize)
+		var y int = rand.Intn(gameBoardSize)
 		fmt.Printf("values: x: %d, y: %d\n", x, y)
 
 		res := doGameStep(gameBoard, x, y, fieldPCCellId)
@@ -201,13 +205,88 @@ func printWinner(winnerNumber int) {
 	d("we have winner")
 }
 
+/*
+ * Calculate game score
+ *
+ */
+func calculateScore(gameBoard [][]gameBoardNode, playerSymbol int) {
+
+	// подсчёт очков для игрока,
+	// у которого символ поля - @playerSymbol
+
+	цикл дял всех ячеек по контуру игрового поля
+	{
+		функция закраски ячейки (i, j, step){
+
+
+			если это ячейка Этого пользователя или захвачена --
+				закрашиваем её().
+				заносим, что это было на ходе @step
+
+				вызываем функцию закраски для всех соседей
+					слева,
+					справа,
+					сверху
+					снизу
+			иначе
+				возврат
+
+
+		}
+	}
+
+	// оставшиеся незакрашенные ячейки - это контура,
+	// которые требуется *захватить*
+	цикл по всем ячейкам поля {
+
+		если текущая ячейка ЗАКРАШЕНА -
+		возврат
+
+		захватить ячейку [i][j]
+
+		// фнкция захвата_ячейки() //
+
+		если это вражеская ячейка,
+			увеличить счёт текущего игрока (+1)
+
+		пометить ячейку как захваченную
+		// TODO: добавить в функцию doGameStep()
+		// 		проверку, захвачена ли ячейка
+		// -------------------------------------- //
+	}
+
+
+
+
+
+}
+
+/*
+ * Detect, if there any winner on game board
+ *
+ */
+func getWinner(gameBoard [][]gameBoardNode) (winner int) {
+	d("get winner")
+
+	/* if emptyCellExist
+	 * 		return (no winner)
+	 * else
+	 * 		return (scoreUser > scorePC) ? userWin : pcWin;
+	 *
+	 * */
+
+
+	winner = 0
+	return
+}
 /* ========================================================================= */
 
 func main() {
 	fmt.Println("\n\t = = = Greeting in 'Dots' game = = =\n");
 
+	time.Sleep(2 * time.Second)
 
-	var mainGameBoard [][]int = initGameBoard(gameBoardSize)
+	var mainGameBoard [][]gameBoardNode = initGameBoard(gameBoardSize)
 
 	var isWin int = 0
 	for /* empty */; isWin == 0; /* empty */ {
@@ -216,6 +295,7 @@ func main() {
 		drawGameBoard(mainGameBoard)
 
 		doUserStep(mainGameBoard);
+		calculateScore()
 
 		clear_screen_linux()
 		drawGameBoard(mainGameBoard);
@@ -225,6 +305,8 @@ func main() {
 		}
 
 		doAIStep(mainGameBoard);
+		calculateScore()
+
 		clear_screen_linux()
 		drawGameBoard(mainGameBoard);
 		isWin = getWinner(mainGameBoard);
